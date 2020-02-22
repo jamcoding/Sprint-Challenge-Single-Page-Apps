be98a957833b111db8d31a6fd2997a3ad5bd68ba
Alex <Magana@Alexs-MacBook-Air.local>
2020-02-21 20:53:49 -0600

    displaying names

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  console.log("character", character);
+  return (
+    <div>
+      <p>{character.name}</p>
+      <p>{character.status}</p>
+      <p>{character.species}</p>
+    </div>
+  );
 }
