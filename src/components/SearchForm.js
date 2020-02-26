423b4b4ca6ab9886e6ef939788edd18ec3aa8ac3
Alex Magana <alex@Alexs-iMac.local>
2020-02-25 20:25:51 -0600

    fixed issues with display

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+export default function SearchForm({characters}) {
+    // console.log("props", character)
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = React.useState(characters);
+
+    const handleChange = event => {
+        setSearchTerm(event.target.value);
+      };
+
+      useEffect(() => {
+          const results = characters.filter(characterObject => 
+            characterObject.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(results);
+      }, [searchTerm]);
+
+    return (
+        <section className="search-form">
+            <form>
+                <label htmlFor="name">Search:</label>
+                <input
+                    placeholder="Search character"
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+            </form>
+
+            <div>
+                {searchResults.map(item => (
+                    <p>{item.name}</p>
+                ))}
+            </div>
+        </section>
+    )
 }
